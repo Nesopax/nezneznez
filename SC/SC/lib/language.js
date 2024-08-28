dcad4507accdfd234689cacabd3e13a5c5e3553c
Bsbwjwkkaoqjwhbwb <ucihagaming50@gmail.com>
2024-08-28 11:43:28 +0800

    Delete SC/SC directory

--- SC/SC/lib/language.js
+++
@@ -1,5 +0,0 @@
-global.nocreator = "Sorry, you are not a Vip Bot user 💀"
-global.notext = 'Include Text 💀'
-global.noingroup = 'The feature will be open if you are in a group💀'
-global.nobotadmin = 'Feature Will Open If Bot Is Admin 💀'
-global.usernoadmin = 'Features Will Be Open If You Are Admin 💀'
--- SC/SC/settings.js
+++
@@ -1,7 +0,0 @@
-global.prefa = ['','!','.',',','🐤','🗿']
-
-global.owner = ['6282223235909']
-global.botname = 'FIKZZ 𝐕𝟐 💀'
-global.baileys1 = require('@whiskeysockets/baileys') 
-global.sticker1 = "'FIKZZ 𝐕𝟐 💀"
-global.sticker2 = "💀"
